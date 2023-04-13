7338c977f3dddd72b3b208fca09c1b39b5273c43
johanCruzHernandez <91350296+CrypticMatter@users.noreply.github.com>
2023-04-13 13:16:21 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import $ from 'jquery';
-import './style.scss';
+import './styles.scss';
 
 // Finds the element with the id of main
 // and changes the content to 'Here we go!'
-$('#main').html('Here we go!');
+
+let num = 0;
+setInterval(() => {
+  $('#main').html(`You've been on this page for ${num} seconds.`);
+  num += 1;
+}, 1000);
